759c8768bb049518191aefc50446ebc7a23728ac
Kelvin <kelvinripnob@gmail.com>
2026-05-15 17:50:34 +0200

    fix firebase and deployment

--- app/firebase.ts
+++ app/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCP-PXwgE2NY7ji15lMtMFELjCdLsZ4oS0",
-  authDomain: "hide-and-seek-a1568.firebaseapp.com",
-  projectId: "hide-and-seek-a1568",
-  storageBucket: "hide-and-seek-a1568.firebasestorage.app",
-  messagingSenderId: "250936024257",
-  appId: "1:250936024257:web:2286d6f2ec5a4eb6398840"
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
 };
 
 const app = initializeApp(firebaseConfig);
